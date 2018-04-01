49c798036f61bc2427e92a7ca21aa33323dc43ce
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-04-01 16:49:18 -0400

    test

--- commands/owner/pull.js
+++ commands/owner/pull.js
@@ -3,8 +3,10 @@ const Discord = require('discord.js');
 
 module.exports.run = function (command, args, message, bot) {
     if(message.author.id == '292377829105205249') {
-        require('child_process').exec('cd /app/ && git pull origin master && refresh', (error, out) => {
-            message.channel.send('```' + out + '```');
+        require('child_process').exec('cd /app/ && git pull origin master', (error, out) => {
+            message.channel.send('```' + out + '```').then((msg) => {
+                require('child_process').exec('refresh', (err, out) => {});
+            })
         });
     }else{
         message.reply('You don\'t own me!');
