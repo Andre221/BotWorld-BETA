0a611546c8462dbe7f8ad70948dfccc8858806ff
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-04-01 16:22:49 -0400

    test

---
+++ commands/owner/pull.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+
+module.exports.run = function (command, args, message, bot) {
+    if(message.author.id == '292377829105205249') {
+        require('child_process').exec('cd /app/ && git pull origin master', (error, out) => {
+            console.log(out);
+        });
+    }else{
+        message.reply('You don\'t own me!');
+    }
+}
+
+module.exports.help = {
+    name: 'pull',
+    aliases: ['p'],
+    usage: '{*}pull',
+    type: 'owner',
+    description: 'Pull from origin/master.'
+}
