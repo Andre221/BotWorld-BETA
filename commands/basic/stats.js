d1afab755f34ec890371d6a2bc37461ec65c4d45
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-04-01 21:16:22 -0400

    stats command

---
+++ commands/basic/stats.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+module.exports.run = function(command, args, message, bot){
+    let embed = new Discord.RichEmbed()
+    .setColor('#AABBED')
+    .setTitle('Bot Stats!')
+    .setDescription(`You may find live updating status on this bot [**__here__**](https://botworld-beta.glitch.me/stats).`)
+    message.channel.send(embed);
+}
+
+module.exports.help = {
+    name: 'stats',
+    aliases: ['info'],
+    usage: '{*}stats',
+    type: 'basic',
+    description: 'Get where you can check my stats **LIVE**.'
+}
+
