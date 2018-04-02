4120f72bf903d361855ab807a2cc2ea294f3e34c
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-04-02 18:35:02 -0400

    test

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -4,11 +4,7 @@ module.exports.run = function(command, args, message, bot){
     let sections = [];
     bot.commands.forEach(cmd => {
         if(!sections.includes(cmd.help.type.split('=>')[0])){
-            if(cmd.help.type.split('=>').length>1 && !sections.includes(cmd.help.type.split('=>')[1])){
-                sections[sections.length] = cmd.help.type.split('=>')[1]
-            }else{
-                sections[sections.length] = cmd.help.type.split('=>')[0];
-            }
+            sections[sections.length] = cmd.help.type.split('=>')[0];
         }
     });
 
