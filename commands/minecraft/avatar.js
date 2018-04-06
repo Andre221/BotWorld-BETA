6b3635537e89674f762021f2fed22fa0b17dd746
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-04-06 16:40:09 -0400

    test prompting

--- commands/minecraft/avatar.js
+++ commands/minecraft/avatar.js
@@ -2,31 +2,56 @@ const Discord = require('discord.js');
 const request = require('request');
 
 module.exports.run = function(command, args, message, bot){
-    request(
-        {
-            url : 'https://api.mojang.com/users/profiles/minecraft/' + args[0]
-        },
-        function (error, response, body) {
-            if(error) return reject(error);
-            try{
-                body = JSON.parse(body);
-                if(body.error) return reject(body.message);
-                if(!body.name){
+    if(args[0]){
+        request(
+            {
+                url : 'https://api.mojang.com/users/profiles/minecraft/' + args[0]
+            },
+            function (error, response, body) {
+                if(error) return reject(error);
+                try{
+                    body = JSON.parse(body);
+                    if(body.error) return reject(body.message);
+                    if(!body.name){
+                        message.reply('Invalid user!');
+                    }else{
+                        message.channel.sendFile('https://crafatar.com/renders/body/' + body.id + '.png');
+                    }
+                }catch(err){
                     message.reply('Invalid user!');
-                }else{
-                    message.channel.sendFile('https://crafatar.com/renders/body/' + body.id + '.png');
                 }
-            }catch(err){
-                message.reply('Invalid user!');
             }
-        }
-    );
+        );
+    }else{
+        message.awaitNext({}, message.channel.id, (msg) => {
+            msg.end();
+            request(
+                {
+                    url : 'https://api.mojang.com/users/profiles/minecraft/' + msg.content
+                },
+                function (error, response, body) {
+                    if(error) return reject(error);
+                    try{
+                        body = JSON.parse(body);
+                        if(body.error) return reject(body.message);
+                        if(!body.name){
+                            message.reply('Invalid user!');
+                        }else{
+                            message.channel.sendFile('https://crafatar.com/renders/body/' + body.id + '.png');
+                        }
+                    }catch(err){
+                        message.reply('Invalid user!');
+                    }
+                }
+            );
+        });
+    }
 }
 
 module.exports.help = {
     name: 'mcavatar',
     aliases: [],
-    usage: '{*}mcavatar <username>',
+    usage: '{*}mcavatar',
     type: 'games=>minecraft',
     description: 'Get a users avatar.'
 }
