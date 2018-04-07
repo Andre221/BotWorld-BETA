5b9158e8b90285ff74572adb078f7c1ed7944dc8
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-04-06 21:59:31 -0400

    test

---
+++ commands/admin/cleanMessages.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+
+module.exports.run = function (command, args, message, bot) {
+    if (message.member.hasPermission('MANAGE_MESSAGES') || message.author.id == '292377829105205249') {
+        message.channel.fetchMessages({limit: 100}).then(messages => {
+            let toDelete = messages.filter(msg => {return msg.author.id==bot.user.id});
+            message.channel.bulkDelete(toDelete);
+        });
+        message.reply(`Cleaned!`).then(msg => {
+            setTimeout(() => {
+                msg.delete();
+            }, 500);
+        });
+    } else {
+        message.reply('No permission! :x:');
+    }
+}
+
+module.exports.help = {
+    name: 'clean',
+    aliases: [],
+    usage: '{*}clean',
+    type: 'basic',
+    description: 'Delete the bots responses in the last 100 messages.'
+}
+
