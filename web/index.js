1cc0b5ceae9f12b6498a77de508622442361a55c
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-04-07 14:23:57 -0400

    test

--- web/index.js
+++ web/index.js
@@ -62,7 +62,7 @@ app.get('/api/stats', (req, res) => {
     res.send({guildCount: bot.guilds.size, uptime: msToTime(bot.uptime), ping: bot.ping, userCount: bot.users.size, messages: bot.messages});
 });
 
-app.get('/api/webhooks/dbl', (req, res) => {
+app.post('/api/webhooks/dbl', (req, res) => {
     if(req.body.Authorization==process.env.DBL_TOKEN){
         console.log('test');
     }else{
