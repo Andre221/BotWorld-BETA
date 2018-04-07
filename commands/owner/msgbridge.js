a0b8e2eefbcd67d4f690f4cbf54aeaeaa467dcb1
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-04-07 11:00:08 -0400

    fix

--- commands/owner/msgbridge.js
+++ commands/owner/msgbridge.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = function (command, args, message, bot) {
     if (message.author.id == '292377829105205249') {
         bot.bridges[message.author.id] = {to: args[0], from: message.channel.id};
-        message.channel.send('Opened bridge to ' + bot.channels.get(args[0]) ? bot.channels[args[0]].name : 'Non-existant channel');
+        message.channel.send('Opened bridge to ' + bot.channels.get(args[0]) ? bot.channels.get(args[0]).name : 'Non-existant channel');
     } else {
         message.reply('You don\'t own me!');
     }
