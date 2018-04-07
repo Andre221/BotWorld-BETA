a0ae2c3e816bb69d804950a50ae84055a39c5fc4
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-04-07 16:33:16 -0400

    test

--- web/images.js
+++ web/images.js
@@ -3,7 +3,8 @@ const express = require('express');
 let router = express.Router();
 
 router.get('/b-top', (req, res) => {
-    res.sendFile(process.cwd() + '/web/images/guille-pozzi-375035-unsplash.jpg');
+    res.redirect('https://www.walldevil.com/wallpapers/a87/space-abstract-patterns-wallpaper-light-outer-backgrounds-wallpapers.jpg');
+    //res.sendFile(process.cwd() + '/web/images/guille-pozzi-375035-unsplash.jpg');
     //res.sendFile(process.cwd() + '/web/images/b-top.png');
 });
 
