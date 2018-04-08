6a852a997ffd4ba7ec3b95fb4ee4bf4351ea8e71
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-04-08 19:35:15 -0400

    test

--- web/index.js
+++ web/index.js
@@ -64,7 +64,7 @@ app.get('/api/stats', (req, res) => {
 
 app.post('/api/webhooks/dbl', (req, res) => {
     if(req.headers.authorization==process.env.DBL_TOKEN){
-        console.log('test');
+        bot.channels.get('265156286406983680').send('vote, ```json\n' + req.body + '```');
     }else{
         res.sendStatus(403);
     }
