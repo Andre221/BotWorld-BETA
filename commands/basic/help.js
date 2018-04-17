6da7eef2e341b48581cbd8814981459e8185d552
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-04-17 11:57:32 -0400

    whoops

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -1,55 +1,57 @@
 const Discord = require('discord.js');
 
-module.exports.run = function (command, args, message, bot) {
+module.exports.run = function(command, args, message, bot){
     let sections = [];
 
     bot.commands.forEach(c => {
-        if (!sections.includes(c.help.type)) sections[sections.length] = c.help.type;
+        if(!sections.includes(c.help.type)) sections[sections.length] = c.help.type;
     });
 
-    if (args[0] && sections.filter(v => v.includes(args[0].toLowerCase())).size>=1) {
-        cmds = [];
-        secs = [];
-        sections.filter(v => v.includes(args[0].toLowerCase())).forEach(sec => {
-            if (sec[sec.length - 1] == args[0].toLowerCase()) {
-                cmds = cmds.concat(bot.commands.filter(v => v.help.type == sec).map(v => v.help));
-            } else {
-                if (sec != [args[0].toLowerCase()]) {
-                    secs[secs.length] = sec[sec.indexOf(args[0].toLowerCase()) + 1];
+    if(args[0] && sections.filter(v=>v.includes(args[0].toLowerCase()))){
+        let cmds = [];
+        let secs = [];
+        sections.filter(v=>v.includes(args[0].toLowerCase())).forEach(sec => {
+            if(sec[sec.length-1]==args[0].toLowerCase()){
+                cmds = cmds.concat(bot.commands.filter(v=>v.help.type==sec).map(v=>v.help));
+            }else{
+                if(sec!=[args[0].toLowerCase()]){
+                    secs[secs.length] = sec[sec.indexOf(args[0].toLowerCase())+1];
                 }
             }
         });
 
 
         let helpEmbed = new Discord.RichEmbed()
-            .setColor('#AABBED')
-            .setTitle('Help | ' + args[0].toLowerCase())
-            if (cmds.length >= 1) {
-                Array.from(new Set(cmds)).forEach(c => {
-                    helpEmbed.addField(c.usage.replace('{*}', 'bb-'), c.description);
-                });
-                if (secs.length >= 1) {
-                    helpEmbed.addBlankField(false);
-                }
-            }
+        .setColor('#AABBED')
+        .setTitle('Help | ' + args[0].toLowerCase())
 
-            if (secs.length >= 1) {
-                Array.from(new Set(secs)).forEach(sec => {
-                    helpEmbed.addField(sec, 'bb-help ' + sec);
-                });
+        if(cmds.length>=1){
+            Array.from(new Set(cmds)).forEach(c=>{
+                helpEmbed.addField(c.usage.replace('{*}', 'b-'), c.description);
+            });
+            if(secs.length>=1){
+                helpEmbed.addBlankField(false);
             }
-            message.channel.send(helpEmbed);
+        }
+
+        if(secs.length>=1){
+            Array.from(new Set(secs)).forEach(sec => {
+                helpEmbed.addField(sec, 'b-help ' + sec);
+            });
+        }
+
+        message.channel.send(helpEmbed);
 
     }else{
         let helpEmbed = new Discord.RichEmbed()
         .setColor('#AABBED')
         .setTitle('Help');
 
-    Array.from(new Set(sections.map(v => v[0]))).forEach(s => {
-        helpEmbed.addField(s, 'bb-help ' + s);
-    });
+        Array.from(new Set(sections.map(v=>v[0]))).forEach(s=>{
+            helpEmbed.addField(s, 'b-help ' + s);
+        });
 
-    return message.channel.send(helpEmbed);
+        message.channel.send(helpEmbed);
     }
 }
 
